0055a82696692276190fbe82ca7ee62bbf60ecec
Max Lettenberger <maxlettenberger@gmail.com>
2018-09-23 15:56:13 -0500

    Add character variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var chewbacca = {
+    name: "Chewbacca",
+    hp: 230,
+    attack: 5,
+    counter: 12,
+    img: "chewbacca.png"
+};
+
+var darthVader = {
+    name: "Darth Vader",
+    hp: 200,
+    attack: 20,
+    counter: 14,
+    img: "darth_vader.jpg"
+};
+
+var maceWindu = {
+    name: "Mace Windu",
+    hp: 190,
+    attack: 19,
+    counter: 16,
+    img: "mace_windu.jpg"
+};
+
+var yoda = {
+    name: "Yoda",
+    hp: 170,
+    attack: 12,
+    counter: 12,
+    img: "yoda.jpg"
+};
+
+
+
